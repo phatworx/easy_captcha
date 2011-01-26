faa420afd7efc9369594a43639ae703b4f305aa0
Marco Scholl <develop@marco-scholl.de>
2011-01-26 22:11:52 +0100

    use new generator class

--- lib/easy_captcha/captcha.rb
+++ lib/easy_captcha/captcha.rb
@@ -19,45 +19,8 @@ module EasyCaptcha
     private
 
     def generate_captcha(file = nil) #:nodoc:
-      canvas = Magick::Image.new(EasyCaptcha.image_width, EasyCaptcha.image_height) do |variable|
-        self.background_color = EasyCaptcha.image_background_color unless EasyCaptcha.image_background_color.nil?
-      end
-
-      # Render the text in the image
-      canvas.annotate(Magick::Draw.new, 0, 0, 0, 0, code) {
-        self.gravity     = Magick::CenterGravity
-        self.font_family = EasyCaptcha.font_family
-        self.font_weight = Magick::LighterWeight
-        self.fill        = EasyCaptcha.font_fill_color
-        if EasyCaptcha.font_stroke.to_i > 0
-          self.stroke       = EasyCaptcha.font_stroke_color
-          self.stroke_width = EasyCaptcha.font_stroke
-        end
-        self.pointsize = EasyCaptcha.font_size
-      }
-
-      # Blur
-      canvas = canvas.blur_image(EasyCaptcha.blur_radius, EasyCaptcha.blur_sigma) if EasyCaptcha.blur?
-
-      # Wave
-      w = EasyCaptcha.wave_length
-      a = EasyCaptcha.wave_amplitude
-      canvas = canvas.wave(rand(a.last - a.first) + a.first, rand(w.last - w.first) + w.first) if EasyCaptcha.wave?
-
-      # Sketch
-      canvas = canvas.sketch(EasyCaptcha.sketch_radius, EasyCaptcha.sketch_sigma, rand(180)) if EasyCaptcha.sketch?
-
-      # Implode
-      canvas = canvas.implode(EasyCaptcha.implode.to_f) if EasyCaptcha.implode.is_a? Float
-
-      # Crop image because to big after waveing
-      canvas = canvas.crop(Magick::CenterGravity, EasyCaptcha.image_width, EasyCaptcha.image_height)
-
-      unless file.nil?
-        canvas.write(file) { self.format = 'PNG' }
-      end
-      @image = canvas.to_blob { self.format = 'PNG' }
-      canvas.destroy!
+      @generator = EasyCaptcha::Generator::Default.new
+      @image = @generator.generate(@code, file)
     end
   end
 end
