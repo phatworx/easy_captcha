053042beb5d8be6ae1d27c52837c15d8c7747c06
Alexander Dreher <alexdreher@lxmedia.net>
2013-07-22 13:34:32 +0200

    cache control for Galaxy S3

--- lib/easy_captcha/controller.rb
+++ lib/easy_captcha/controller.rb
@@ -1,6 +1,7 @@
 module EasyCaptcha
   # captcha controller
   class Controller < ActionController::Base
+    before_filter :overwrite_cache_control
     # captcha action send the generated image to browser
     def captcha
       if params[:format] == "wav" and EasyCaptcha.espeak?
@@ -8,6 +9,14 @@ module EasyCaptcha
       else
         send_data generate_captcha, :disposition => 'inline', :type => 'image/png'
       end
+    end
+
+    private
+    # Overwrite cache control for Samsung Galaxy S3 (remove no-store)
+    def overwrite_cache_control
+      response.headers["Cache-Control"] = "no-cache, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
     end
   end
 end
