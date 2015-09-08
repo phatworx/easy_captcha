8e8868bc23ddf7f790dbdcd30ae6fd68c174c04d
Alexander Dreher <alexdreher@lxmedia.net>
2015-09-08 09:45:34 +0200

    fixes espeak specs

--- spec/espeak_spec.rb
+++ spec/espeak_spec.rb
@@ -2,23 +2,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe EasyCaptcha::Espeak do
-  context "check default values" do
+  context 'check default values' do
     subject { EasyCaptcha::Espeak.new }
 
-    its(:amplitude) { (80..120).to_a.should include(subject) }
-    its(:pitch) { (30..70).to_a.should include(subject) }
+    its(:amplitude) { (80..120).should include(subject.amplitude) }
+    its(:pitch) { (30..70).should include(subject.pitch) }
     its(:gap) { should eq 80 }
     its(:voice) { should be_nil }
-
   end
 
-  context "check config: voices" do
+  context 'check config: voices' do
     let(:voices) { ['german', 'german+m1'] }
     subject do
       EasyCaptcha::Espeak.new do |config|
         config.voice = voices
       end
     end
-    its(:voice) { voices.should include(subject) }
+    it { voices.should include(subject.voice) }
   end
 end
