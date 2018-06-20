6f3487b8858af8c8eb6bcf6109b452ac2edc0cb7
Bruno Porto <brunotporto@gmail.com>
2018-06-20 14:00:09 -0300

    change captcha_url to captcha_path

--- lib/easy_captcha/view_helpers.rb
+++ lib/easy_captcha/view_helpers.rb
@@ -5,7 +5,7 @@ module EasyCaptcha
     def captcha_tag(*args)
       options = { :alt => 'captcha', :width => EasyCaptcha.image_width, :height => EasyCaptcha.image_height }
       options.merge! args.extract_options!
-      image_tag(captcha_url(:i => Time.now.to_i), options)
+      image_tag(captcha_path(:i => Time.now.to_i), options)
     end
   end
 end
